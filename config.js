d4b3ca805658b3915eaae0fd82cfc3f969c7d2d6
splitsecond7545 <72669258+splitsecond7545@users.noreply.github.com>
2021-06-20 15:11:30 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCVXDo4UsCoV1ybV2jBDjvm47SHEKwQEfo",
+    authDomain: "pro-60-87dee.firebaseapp.com",
+    databaseURL: "https://pro-60-87dee-default-rtdb.firebaseio.com",
+    projectId: "pro-60-87dee",
+    storageBucket: "pro-60-87dee.appspot.com",
+    messagingSenderId: "892009752367",
+    appId: "1:892009752367:web:6830d018433afefe0ca3da"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
